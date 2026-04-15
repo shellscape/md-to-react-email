4cb97c8c6b95896aad59d57cced7a1cef1bb9ac1
CharlieHelps <charlie@charlielabs.ai>
2026-04-15 14:13:12 +0000

    fix: escape single quotes in html attrs

--- src/utils.ts
+++ src/utils.ts
@@ -14,6 +14,7 @@ function escapeHtmlAttr(value: string): string {
   return value
     .replace(/&/g, "&amp;")
     .replace(/"/g, "&quot;")
+    .replace(/'/g, "&#39;")
     .replace(/</g, "&lt;")
     .replace(/>/g, "&gt;");
 }
